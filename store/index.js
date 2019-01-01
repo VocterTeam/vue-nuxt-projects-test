89d20487b52f7a398c239e73d273342207b360a1
Arthur <arthur.mirzoyan@vocter.com>
2019-01-01 04:38:54 +0200

    test heroku

--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
-const cookieparser = process.server ? require('cookieparser') : undefined
+// const cookieparser = process.server ? require('cookieparser') : undefined
 
 export const state = () => ({
 	apiUrls: {
@@ -23,16 +23,16 @@ export const actions = {
 		commit('setAuth', auth)
 	},
 
-	nuxtServerInit({ commit }, { req }) {
-	 let auth = null
-	  if (req && req.headers && req.headers.cookie) {
-	    const parsed = cookieparser.parse(req.headers.cookie)
-	    try {
-	      auth = JSON.parse(parsed.auth)
-	    } catch (err) {
-	      // No valid cookie found
-	    }
-	  }
-	  commit('setAuth', auth)
-	}
+	// nuxtServerInit({ commit }, { req }) {
+	//  let auth = null
+	//   if (req && req.headers && req.headers.cookie) {
+	//     const parsed = cookieparser.parse(req.headers.cookie)
+	//     try {
+	//       auth = JSON.parse(parsed.auth)
+	//     } catch (err) {
+	//       // No valid cookie found
+	//     }
+	//   }
+	//   commit('setAuth', auth)
+	// }
 }
